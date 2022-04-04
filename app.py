115ff5c58c7cf7f9707148d305e37592bc918d40
esir <esirkings@gmail.com>
2022-04-04 17:42:29 +0300

    Adds flask_migrate and add migrations

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 
 from healthtools_ec.core import app
 from healthtools_ec.models import db
 
 migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
